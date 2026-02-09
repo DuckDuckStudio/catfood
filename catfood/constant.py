47f50069381c5a17ba391168c517bc30aabbdc51
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2026-02-09 21:59:41 +0800

    feat(constant): 新增 YES 和 NO 常量用于代表 yes/no 的一些输入 (#6)

--- catfood/constant.py
+++ catfood/constant.py
@@ -1,5 +1,5 @@
 """
-catfood 的一些常量
+一些常量
 """
 
 from typing import Final
@@ -7,4 +7,25 @@ from typing import Final
 VERSION: Final = "1.0.8"
 """
 catfood 的版本。
+"""
+
+YES = (
+    "yes", "y",
+    "true", "t",
+    "要",
+    "是"
+)
+"""
+代表 `yes` 的一些输入
+"""
+
+NO = (
+    "no", "n",
+    "false", "f",
+    "否",
+    "不",
+    "不要", "不是"
+)
+"""
+代表 `no` 的一些输入
 """
