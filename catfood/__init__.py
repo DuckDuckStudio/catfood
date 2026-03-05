46265187d19f7d0d80f3f713a4d1ff7c26e74097
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2026-03-05 14:52:48 +0800

    depreacted!: 不再提升接口级别 (#19)

--- catfood/__init__.py
+++ catfood/__init__.py
@@ -5,31 +5,5 @@ Cat Food - A collection of various commonly used functions.
 """
 
 from .constant import VERSION
-from .exceptions.operation import (CancelOther, OperationFailed,
-                                   OperationNotSupported, TryOtherMethods)
-from .exceptions.request import RequestException
-from .functions.files import open_file
-from .functions.format.github import IssueNumber, ResolvesIssue
-from .functions.github.api import 获取GitHub文件内容, 请求GitHubAPI
-from .functions.github.token import read_token, 这是谁的Token
-from .functions.print import 消息头
-from .functions.terminal import calculateCharactersDisplayed
 
 __version__ = VERSION
-__all__ = [
-    "VERSION",
-    "消息头",
-    "open_file",
-    "calculateCharactersDisplayed",
-    "IssueNumber",
-    "ResolvesIssue",
-    "获取GitHub文件内容",
-    "请求GitHubAPI",
-    "read_token",
-    "这是谁的Token",
-    "OperationFailed",
-    "TryOtherMethods",
-    "CancelOther",
-    "OperationNotSupported",
-    "RequestException",
-]
