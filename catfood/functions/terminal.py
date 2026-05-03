697964ecaa8a02a42c0a367ed9fa7b4884430ead
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2026-05-03 21:13:40 +0800

    refactor(func/terminal): 将 KeyboardInterrupt 的 try...except 统一到 while 里面

--- catfood/functions/terminal.py
+++ catfood/functions/terminal.py
@@ -28,47 +28,46 @@ def runCommand(command: list[str] | str, retry: int = -1) -> int:
     if isinstance(command, str):
         command = command.split(" ")
 
-    try:
-        while True:
-            try:
-                result = subprocess.run(command, capture_output=True, text=True)
-                if result.stdout.strip():
-                    print(result.stdout.strip())
-                if result.stderr.strip():
-                    print(result.stderr.strip())
+    while True:
+        try:
+            result = subprocess.run(command, capture_output=True, text=True)
+            if result.stdout.strip():
+                print(result.stdout.strip())
+            if result.stderr.strip():
+                print(result.stderr.strip())
 
-                if result.returncode == 0:
-                    return 0
+            if result.returncode == 0:
+                return 0
+            else:
+                print(f"{消息头.错误} 运行 {Fore.BLUE}{' '.join(command)}{Fore.RESET} 失败，{command[0]} 返回非零退出代码 {Fore.BLUE}{result.returncode}{Fore.RESET}")
+
+                if retry < 0:
+                    return result.returncode
                 else:
-                    print(f"{消息头.错误} 运行 {Fore.BLUE}{' '.join(command)}{Fore.RESET} 失败，{command[0]} 返回非零退出代码 {Fore.BLUE}{result.returncode}{Fore.RESET}")
+                    if command[0] == "git":
+                        if not any(keyword in result.stderr.lower() for keyword in (
+                            "unable to access", "could not resolve host",
+                            "failed to connect", "operation timed out",
+                            "early eof", "rpc failed"
+                        )):
+                            print(f"{消息头.警告} 这看起来像是 Git 遇到了网络之外的问题，拒绝重试")
+                            return result.returncode
 
-                    if retry < 0:
-                        return result.returncode
-                    else:
-                        if command[0] == "git":
-                            if not any(keyword in result.stderr.lower() for keyword in (
-                                "unable to access", "could not resolve host",
-                                "failed to connect", "operation timed out",
-                                "early eof", "rpc failed"
-                            )):
-                                print(f"{消息头.警告} 这看起来像是 Git 遇到了网络之外的问题，拒绝重试")
-                                return result.returncode
+                    if retry > 0:
+                        try:
+                            for i in reversed(range(1, retry+1)):
+                                print(f"\r{i}秒后重试...", end="")
+                                time.sleep(1)
+                        finally:
+                            print("\r", end="")
 
-                        if retry > 0:
-                            try:
-                                for i in reversed(range(1, retry+1)):
-                                    print(f"\r{i}秒后重试...", end="")
-                                    time.sleep(1)
-                            finally:
-                                print("\r", end="")
-            except FileNotFoundError:
-                print(f"{消息头.错误} 未找到 {command[0]}")
-                return 1
-
-            print(f"{消息头.信息} 正在重试 ...")
-    except KeyboardInterrupt as e:
-        print(f"{消息头.错误} 终止运行命令 {Fore.BLUE}{' '.join(command)}{Fore.RESET}，因为收到了 Ctrl + C (KeyboardInterrupt)")
-        raise KeyboardInterrupt from e
+                    print(f"{消息头.信息} 正在重试 ...")
+        except FileNotFoundError:
+            print(f"{消息头.错误} 未找到 {command[0]}")
+            return 1
+        except KeyboardInterrupt as e:
+            print(f"{消息头.错误} 终止运行命令 {Fore.BLUE}{' '.join(command)}{Fore.RESET}，因为收到了 Ctrl + C (KeyboardInterrupt)")
+            raise KeyboardInterrupt from e
 
 def calculateCharactersDisplayed(content: str) -> int:
     """
