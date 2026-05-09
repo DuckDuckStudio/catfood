8955bf8fd65c85d4cd0a338a95257027f6fb1f78
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2026-05-09 17:47:38 +0800

    docs(func/terminal): 使用原样 URL 以避免 Pylint 的 line-too-long

--- catfood/functions/terminal.py
+++ catfood/functions/terminal.py
@@ -73,7 +73,7 @@ def calculateCharactersDisplayed(content: str) -> int:
     """
     计算内容在 Windows 终端上显示占多少字符的位置。
 
-    方法请参阅我的文章: https://duckduckstudio.github.io/Articles/#/%E4%BF%A1%E6%81%AF%E9%80%9F%E6%9F%A5/Python/%E8%BE%93%E5%87%BA/%E8%AE%A1%E7%AE%97%E8%BE%93%E5%87%BA%E7%9A%84%E5%86%85%E5%AE%B9%E5%9C%A8Windows%E7%BB%88%E7%AB%AF%E4%B8%8A%E7%9A%84%E6%98%BE%E7%A4%BA%E5%8D%A0%E5%A4%9A%E5%B0%91%E5%AD%97%E7%AC%A6
+    方法请参阅我的文章: https://duckduckstudio.github.io/Articles/#/信息速查/Python/输出/计算输出的内容在Windows终端上的显示占多少字符
     
     :param content: 指定的内容
     :type content: str
