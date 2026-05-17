c4d5880ce1950dc73086570ae8a62307a676bad9
鸭鸭「カモ」 <89643991+DuckDuckStudio@users.noreply.github.com>
2026-05-18 00:23:17 +0800

    types(func/format/github): 为 ResolvesIssue 函数的 string 参数的类型注释添加 int | None
    Resolves #15，该函数内部通过 `IssueNumber(...) -> str | None` 函数实现，这个函数早就已经接受了 int | None 输入。

--- catfood/functions/format/github.py
+++ catfood/functions/format/github.py
@@ -6,7 +6,7 @@ def IssueNumber(string: str | int | None) -> str | None:
     """
     从给定的字符串中获取 Issue 或 PR 的编号。
     
-    :param string: 给定的字符串或整数或 None
+    :param string: 给定的输入内容
     :type string: str | int | None
     :return: 返回字符串的编号，获取失败返回 None
     :rtype: str | None
@@ -36,14 +36,14 @@ def IssueNumber(string: str | int | None) -> str | None:
 
     return None
 
-def ResolvesIssue(string: str, keyword: str = "Resolves") -> str | None:
+def ResolvesIssue(string: str | int | None, keyword: str = "Resolves") -> str | None:
     """
     将给定的字符串格式化为 GitHub PR 的 Resolves Issue 格式
 
     GitHub Docs: https://docs.github.com/zh/issues/tracking-your-work-with-issues/using-issues/linking-a-pull-request-to-an-issue#linking-a-pull-request-to-an-issue-using-a-keyword
     
-    :param string: 给定的字符串
-    :type string: str
+    :param string: 给定的输入内容
+    :type string: str | int | None
     :param keyword: 链接议题时使用的关键词
     :type keyword: str
     :return: 格式化成功返回字符串结果，失败返回 None
